d040e076a18bf3de332fff4bc30e6b883affd634
Rahul Johny <rahul.johny@tcs.com>
2017-10-19 18:18:18 +0530

    First commit

---
+++ src/main/java/com/controller/TestController.java
@@ -0,0 +1,22 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by online09 on 10/19/2017.
+ */
+
+@Controller
+public class TestController {
+
+    @RequestMapping(value = "/testPage", method = RequestMethod.GET)
+    public String printTestPage(ModelMap model) {
+        model.addAttribute("Message", "This is Rahul Johny");
+
+        return "testPage";
+    }
+
+}
